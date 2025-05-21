e5b554681d74354f3d3cc12398b6dcb58055a32a
Jose Carlos Giron <josecarlosgironsolano@gmail.com>
2025-05-21 17:32:55 -0400

    feat(ubicaciones): añadir generacion de rutas entre dos puntos

--- src/main/java/com/grupoG/ProyectoSIG/services/DistribuidorService.java
+++ src/main/java/com/grupoG/ProyectoSIG/services/DistribuidorService.java
@@ -1,6 +1,8 @@
 package com.grupoG.ProyectoSIG.services;
 
+import com.grupoG.ProyectoSIG.dto.UbicacionDTO;
 import com.grupoG.ProyectoSIG.models.Distribuidor;
+import com.grupoG.ProyectoSIG.models.Ubicacion;
 import com.grupoG.ProyectoSIG.repositories.DistribuidorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -33,5 +35,16 @@ public class DistribuidorService {
 
     public Optional<Distribuidor> getClienteById(Long id) {
         return distribuidorRepository.findById(id);
+    }
+
+    public Optional<Distribuidor> getMasCercano(UbicacionDTO ubicacion){
+        Distribuidor cercano = distribuidorRepository.findAll().getFirst();
+        return distribuidorRepository.findById(cercano.getId());
+    }
+
+    public UbicacionDTO getUbicacionById(Long id){
+        Distribuidor distribuidor = findById(id).orElseThrow();
+        UbicacionDTO ubicacion = new UbicacionDTO(-15.878787,-64.2323);
+        return ubicacion;
     }
 }
--- src/main/java/com/grupoG/ProyectoSIG/services/PedidoService.java
+++ src/main/java/com/grupoG/ProyectoSIG/services/PedidoService.java
@@ -1,7 +1,12 @@
 package com.grupoG.ProyectoSIG.services;
 
+import com.grupoG.ProyectoSIG.dto.RutaDTO;
+import com.grupoG.ProyectoSIG.dto.UbicacionDTO;
+import com.grupoG.ProyectoSIG.models.Distribuidor;
 import com.grupoG.ProyectoSIG.models.Pedido;
+import com.grupoG.ProyectoSIG.models.Ubicacion;
 import com.grupoG.ProyectoSIG.repositories.PedidoRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +17,34 @@ public class PedidoService {
     @Autowired
     private PedidoRepository pedidoRepository;
 
+    @Autowired
+    private RutaService rutaService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+
+    @Autowired
+    private DistribuidorService distribuidorService;
+
     public <S extends Pedido> S save(S entity) {
+        UbicacionDTO origen = modelMapper.map(entity.getDireccion_envio(),UbicacionDTO.class);
+        Distribuidor distribuidorCercano = distribuidorService.getMasCercano(origen).orElseThrow();
+        RutaDTO ruta = rutaService.calcularRuta(origen, distribuidorService.getUbicacionById(distribuidorCercano.getId()));
         return pedidoRepository.save(entity);
     }
 
     public List<Pedido> findAll(){
         return pedidoRepository.findAll();
     }
+
+    public RutaDTO asignarDistribuidor(Pedido pedido, Distribuidor distribuidor){
+
+        UbicacionDTO origen = modelMapper.map(pedido.getDireccion_envio(),UbicacionDTO.class);
+        Distribuidor distribuidorCercano = distribuidorService.getMasCercano(origen).orElseThrow();
+
+        return rutaService.calcularRuta(origen, distribuidorService.getUbicacionById(distribuidorCercano.getId()));
+    }
+
+
 }
